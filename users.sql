5a8d867ca50b0435d157c84d0877c3ba36ef2c61
ASeeto <seeto.alexander@gmail.com>
2015-06-11 18:31:10 -0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,42 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.3.8
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 10, 2015 at 02:34 PM
--- Server version: 5.5.42-37.1
--- PHP Version: 5.4.23
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `project`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE IF NOT EXISTS `users` (
   `email` char(100) COLLATE utf8_unicode_ci NOT NULL,
   `password` char(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`email`, `password`) VALUES
-('test@test.com', 'test')
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+)
