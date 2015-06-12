d8073177c8d1bc875a40eaa1bb6a2ca3a68eb0cd
ASeeto <seeto.alexander@gmail.com>
2015-06-11 20:12:03 -0400

    Fixed variables' case sensitivity.

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ $.ajaxSetup({
     statusCode: {
         401: function(){
             // Redirec the to the login page.
-            window.location.replace(baseURL+project+'/#login');
+            window.location.replace(BASEURL+PROJECT+'/#login');
          
         },
         403: function() {
             // 403 -- Access denied
-            window.location.replace(baseURL+project+'/#denied');
+            window.location.replace(BASEURL+PROJECT+'/#denied');
         }
     }
 });
